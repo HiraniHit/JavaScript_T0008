9dc50abe60d5d5302d73f0950220396c2c6912bb
DESKTOP-NQIR9PA\ADMIN <hit.devoteeinfotech@gamil.com>
2024-07-15 14:34:12 +0530

    group 1

---
+++ javascript_Theory/Group 1/operators.js
@@ -0,0 +1,99 @@
+console.log("5");
+
+//Arithmatic Operators
+
+console.log("5 :", "Arithmatic Operators ( + , - , * , / , % , ++ , --)");
+
+let operator1 = 10;
+let operator2 = 20;
+
+console.log("5 :","let operator1 = 10");
+console.log("5 :","let operator2 = 20");
+console.log("5 : + :",operator1 + operator2);
+console.log("5 : - :",operator1 - operator2);
+console.log("5 : * :",operator1 * operator2);
+console.log("5 : / :",operator1 / operator2);
+console.log("5 : % :",operator2 % operator1);
+console.log("5 : ++ :",operator1++);
+console.log("5 : ++ :",operator1);
+console.log("5 : -- :",operator2--);
+console.log("5 : -- :",operator2);
+
+
+//Comparison(Relational) Operators
+
+console.log("5 :", "Comparison(Relational) Operators ( == , === , != ,!== , > , >= , < , <=)");
+
+let operator3 = 20;
+let operator4 = "20";
+let operator5 = 30
+
+console.log("5 :","let operator3 = 20");
+console.log("5 :","let operator4 = '20'");
+console.log("5 : == :",operator3 == operator4);
+console.log("5 : === :",operator3 === operator4);
+console.log("5 : != :",operator3 != operator4);
+console.log("5 : !== :",operator3 !== operator4);
+console.log("5 : > :",operator3 > operator4);
+console.log("5 : < :",operator3 < operator4);
+console.log("5 : >= :",operator3 >= operator4);
+console.log("5 : <= :",operator3 <= operator4);
+
+
+//Bitwise Operators
+
+console.log("5 :", "Bitwise Operators ( & , | , ^ , ~ , << , >> , >>>)");
+
+let operator6 = 10;
+let operator7 = 30;
+
+console.log("5 :","let operator6 = 10");
+console.log("5 :","let operator7 = '30'");
+console.log("5 : & :",operator6 & operator7 );
+console.log("5 : | :",operator6 | operator7 );
+console.log("5 : ^ :",operator6 ^ operator7 );
+console.log("5 : ~ :", ~ operator6 );
+console.log("5 : << :",operator6 << operator7 );
+console.log("5 : >> :",operator6 >> operator7 );
+console.log("5 : >>> :",operator6 >>> operator7 );
+
+//Logical Operators
+
+console.log("5 :", "Logical Operators ( && , || , !)");
+
+let operator8 = 10;
+let operator9 = 30;
+
+console.log("5 :","let operator8 = 10");
+console.log("5 :","let operator9 = '30'");
+console.log("5 : && :", operator8 && operator9);
+console.log("5 : || :", operator8 || operator9);
+console.log("5 : ! :", !operator9);
+
+//Assignment Operators
+
+console.log("5 :", "Assignment Operators ( = , += , -= , /= , %=)");
+
+let operator10 = 10;
+let operator11 = 20;
+
+console.log("5 :","let operator10 = 10");
+console.log("5 :","let operator11 = 20");
+
+console.log("5 : = :",operator10 = operator11);
+console.log("5 : += :",operator10 += operator11);
+console.log("5 : -= :",operator10 -= operator11);
+console.log("5 : /= :",operator10 /= operator11);
+console.log("5 : %= :",operator10 %= operator11);
+
+//Special Operators
+
+console.log("5 :", "Special Operators ( ? , : )");
+
+let operator12 = 10;
+let operator13 = 30;
+
+console.log("5 :","let operator12 = 10");
+console.log("5 :","let operator13 = 30");
+console.log("5 : ? :",true ? operator12 : operator13);
+console.log("5 : : :",false ? operator12 : operator13);
