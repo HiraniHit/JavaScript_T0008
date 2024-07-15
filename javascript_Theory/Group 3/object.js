670e6fa9b92f8f96171705c6bfd06855ca865da5
DESKTOP-NQIR9PA\ADMIN <hit.devoteeinfotech@gamil.com>
2024-07-15 14:39:04 +0530

    group 3

---
+++ javascript_Theory/Group 3/object.js
@@ -0,0 +1,10 @@
+console.log("1");
+
+let obj = {
+    name : "obj" , 
+    description : "object is sontain many things",
+    arr : [1,2,3,4,5],
+    progress : true,
+    number :10
+}
+console.log("1 :",obj);
